b85a3dbb1a7d5873a7ab401907196bc5a8daf20b
dherrera1911 <dherrera1911@gmail.com>
2020-07-08 00:29:55 -0300

    Adds to contact tracing, and generates new file for simulating dynamics

---
+++ Alle_dynamics_simulation.R
@@ -0,0 +1,179 @@
+###################################################################
+##### Dynamic
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(segmented)
+
+# simulation parameters
+
+set.seed(2691)
+nRep <- 200
+minI <- 10
+maxI <- 1000
+p_se <- 0.8 # sin subexponencial
+#p_se=0.8 # subexponencial
+
+# SIR simulation with Allee effect
+SIR_Allee <- function(I0, betaMax=1.4, gammaMax=5, p, durSim,
+                                  Allee = TRUE, migration = 1,
+                                  dispersion = 0.2, I50, Nsus = 100000){
+  S <- rep(NA, durSim)
+  I <- rep(NA, durSim)
+  R <- rep(NA, durSim)
+  newI <- rep(NA, durSim)
+  Imported <- rep(NA, durSim)
+  # assign initial values
+  S[1] <- Nsus
+  I[1] <- I0
+  R[1] <- 0
+  newI[1] <- 0
+  Imported[1] <- 0
+  Npop <- Nsus + I0
+  # preassign params for non-Allee
+  beta <- betaMax
+  gamma <- gammaMax
+  for(t in 2:durSim){
+    if (I[t-1] <= 0) {
+      I[t-1] <- I[t] <- 0
+    }
+    if(S[t-1] <= 0) {
+      S[t-1] <- S[t] <- 0
+    }
+    if (Allee == T) {
+      # adjust beta and gamma to infected levels if Allee
+      beta <- betaMax * I[t-1] / (I[t-1] + I50)
+      gamma <- gammaMax * I[t-1] / (I[t-1] + I50)
+    }
+    # get expected changes 
+    newIExpected <- beta * S[t-1] * (I[t-1])^p / Npop
+    recoveredIExpected <- (1/gamma) * I[t-1]
+    # sample the actual changes from random variables
+    newISample <- rpois(n = 1, lambda  = newIExpected)
+    newI[t] <- min(newISample, S[t-1]) # ensure no more new infections than susceptible
+    recoveredI <- rnbinom(n = 1, size = dispersion, mu = recoveredIExpected)
+    recoveredI <- min(I[t-1], recoveredI, na.rm=T) # no more recovered than infected
+    Imported[t] <- rnbinom(n = 1, size = dispersion, mu = migration)
+    #new.I<-rnbinom(n = 1, size = dispersion, mu = new.I)
+    S[t] <- S[t-1] - newI[t]
+    I[t] <- I[t-1] + newI[t] + Imported[t] - recoveredI
+    R[t] <- R[t-1] + recoveredI
+    #out[i,]<-c(out[t-1,1]-new.I, out[t-1,2]+new.I-recoveredI+imported,out[t-1,3]+recoveredI, new.I, imported)
+    }
+  # store results in dataframe
+  out <- data.frame(t = c(1:length(S)), S = S, I = I, R = R,
+                    newI = newI, Imported = Imported)
+  return(out)
+}
+
+# generate repetitions of simulations
+SIR_generator <- function(p_se, allee, nRep) {
+  outputDf <- data.frame()
+  for (al in allee) {
+    rep <- 1
+    while (rep <= nRep) {
+      sim <- SIR_Allee(I0=10, betaMax=0.8, gammaMax=3 , p=p_se,
+                                   durSim=100,  migration=1, dispersion=0.2,
+                                   I50=10, Nsus=100000, Allee = al)
+      sim <- dplyr::mutate(sim, cumI = cumsum(newI),
+                           cumImported = cumsum(Imported),
+                           rep = rep, allee = al, p = p_se) %>%
+        dplyr::filter(., cumI > minI & cumI < maxI)
+      sim$t <- c(1:nrow(sim))
+      if (nrow(sim) > 10) {
+        outputDf <- rbind(outputDf, sim)
+        rep <- rep + 1
+      }
+    }
+  }
+  return(outputDf)
+}
+
+# fit segmented lm to dynamics of single run
+fit_segmented <- function(cumI) {
+    # fit segmented model  
+    cumI <- log10(cumI)
+    t <- log10(c(1:length(cumI)))
+    fit <- segmented(lm(cumI ~ t), seg.Z = ~t, npsi =1)
+    cfs <- summary(fit)
+    slopeVals <- cfs$coefficients[,1]
+    if (length(slopeVals) <= 2) {
+      slopeVals <- c(slopeVals[1:2], NA)
+    } else {
+      slopeVals <- slopeVals[1:3]
+    }
+    breakingPoint <- ifelse(is.null(cfs$psi), NA, cfs$psi[2])
+    coefficients <- c(slopeVals, breakingPoint)
+    # check if breaking point is significant by fitting lm
+    lmFit <- lm(cumI ~ t)
+    pValue <- davies.test(lmFit)$p.value
+    # put together
+    coefficients <- as.list(c(coefficients, pValue))
+    names(coefficients) <- c("intercept", "slopeI", "slopeF",
+                             "breakPoint", "pVal")
+  return(coefficients)
+}
+
+
+#### Run simulations and plot data ######
+allee <- c(TRUE, FALSE)
+simulations <- SIR_generator(p_se = p_se, allee = allee, nRep = nRep)
+
+fitCoefs <- group_by(simulations, allee, rep, p) %>%
+  dplyr::summarise(., coefs = list(fit_segmented(cumI))) %>%
+  ungroup(.) %>%
+  tidyr::unnest_wider(., coefs) %>%
+  dplyr::mutate(., cociente = slopeF/slopeI, resta = slopeF - slopeI,
+                angle = atan(abs((slopeF - slopeI)/(1-slopeF*slopeI))),
+                Ithreshold = intercept + slopeI*breakPoint) %>%
+  dplyr::filter(., cociente < 5000 & cociente > 0)
+
+plotChange <- fitCoefs %>%
+  dplyr::mutate(., allee = c("w/o Allee", "Allee")[as.integer(allee)+1]) %>%
+  ggscatterhist(., x = "slopeI", y = "slopeF", color = "allee",
+                alpha = 0.5, size = 3, margin.plot = "boxplot",
+                palette = c("#b33018", "#14b74b"),
+                ggtheme = theme_bw(), xlab="Initial slope",
+                ylab="Slope after threshold",
+                legend.title = "",
+                margin.params = list(color = "allee"))
+
+sampleRepetitions <- sample(unique(simulations$rep), nPlotDyn)
+
+plotsDynAllee <- dplyr::filter(simulations, allee == TRUE & rep %in% sampleRepetitions) %>%
+  ggplot(., aes(x = t, y = cumI)) +
+  geom_point(color = "#b33018") +
+  facet_wrap(~rep, scales = "free", ncol = 4) +
+  scale_x_continuous(trans = "log10", breaks = c(1, 5, 25)) +
+  scale_y_continuous(limits = c(10, NA), trans = "log10") +
+  xlab("time (days)") +
+  ylab("Cumulative infected") +
+  ggtitle("Allee") +
+  theme_classic() +
+  theme(strip.background = element_blank(), strip.text.x = element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
+  NULL
+  
+
+plotsDynNonAllee <- dplyr::filter(simulations, allee == FALSE & rep %in% sampleRepetitions) %>%
+  ggplot(., aes(x = t, y = cumI)) +
+  geom_point(color = "#14b74b") +
+  facet_wrap(~rep, scales = "free", ncol = 4) +
+  scale_x_continuous(trans = "log10", breaks = c(1, 5, 25)) +
+  scale_y_continuous(limits = c(10, NA), trans = "log10") +
+  xlab("time (days)") +
+  ylab("Cumulative infected") +
+  ggtitle("W/o Allee") +
+  theme_classic() +
+  theme(strip.background = element_blank(), strip.text.x = element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
+  NULL
+  
+dynsPlots <- grid.arrange(plotsDynAllee, plotsDynNonAllee, ncol=2)
+
+ggsave("simDynamics.png", dynsPlots, width = 20, height = 12, units = "cm")
+ggsave("slopeAnalysis.png", width = 20, height = 12, units = "cm")
+
+
