337f60218a84923d658631e666716b632af0a2e0
Álvaro Cabana <almadana@gmail.com>
2020-07-09 11:54:37 -0300

    fig 2 slopes con cruces

--- plotCondados.r
+++ plotCondados.r
@@ -40,7 +40,7 @@ load('simulados_allee.RData')
 # 
 
 
-bb$allee.f=factor("U.S. counties",levels = c(levels(cuadrantes$allee.f),"U.S. counties"))
+bb$allee.f=factor("U.S. counties",levels = c("U.S. counties",levels(cuadrantes$allee.f)))
 
 xlims=c(0,.5)
 ylims= c(0,2)
@@ -87,9 +87,11 @@ cuadrantes$Id=seq(10000,10000+nrow(cuadrantes)-1,1)
 bb$allee.f="U.S. counties"
 sim_bb = merge(cuadrantes[,c("Id","n","allee.f","initial.slope","slope.after.thresh","cociente")],bb,by=c("Id","allee.f","initial.slope","slope.after.thresh","cociente"),all=T)
 View(sim_bb)
+sim_bb$allee.f=relevel(sim_bb$allee.f,"U.S. counties")
 
-xlims=c(0,.5)
-ylims= c(-.01,2.5)
+
+xlims=c(0,.35)
+ylims= c(-.01,2)
 
 plot_slopes=
   sim_bb %>% mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
@@ -98,86 +100,96 @@ plot_slopes=
   ggscatterhist(x="initial.slope_log10",y="cociente_log10",
                 #color="bajoUmbral",
                 color="allee.f",
-                alpha=.4,size=3,
+                fill="allee.f",
+                shape="allee.f",
+                size="allee.f",
+                alpha="allee.f",
+                alpha=.4,
                 margin.plot="boxplot",
                 ggtheme=theme_bw(),
                 xlab="log10(1+initial slope)",
                 ylab="log10(1+slope after threshold/initial slope)",
-                palette = c("#b33018","#14b74b","#000080"),
+                palette = c("#000080","#b33018","#14b74b"),
                 ylim=ylims,
                 xlim=xlims
   )
 
-
-plot_slopes_solo_condados=
-  sim_bb %>% filter(allee.f=="U.S. counties") %>% 
-  mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
-  #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
-  filter(cociente>1) %>% 
-  ggscatterhist(x="initial.slope_log10",y="cociente_log10",
-                #color="bajoUmbral",
-                color="allee.f",
-                alpha=.4,size=3,
-                margin.plot="boxplot",
-                ggtheme=theme_bw(),
-                xlab="log10(1+initial slope)",
-                ylab="log10(1+slope after threshold/initial slope)",
-                palette = c("#000080"),
-                ylim=ylims,
-                xlim=xlims
-  )
-
-plot_slopes_con_allee=
-  sim_bb %>% filter(allee.f=="with Allee effect") %>% 
-  mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
-  #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
-  filter(cociente>1) %>% 
-  ggscatterhist(x="initial.slope_log10",y="cociente_log10",
-                #color="bajoUmbral",
-                color="allee.f",
-                alpha=.4,size=3,
-                margin.plot="boxplot",
-                ggtheme=theme_bw(),
-                xlab="log10(1+initial slope)",
-                ylab="log10(1+slope after threshold/initial slope)",
-                palette = c("#b33018"),
-                ylim=ylims,
-                xlim=xlims
-  )
-
-plot_slopes_con_allee$sp
-
-plot_slopes_sin_allee=
-  sim_bb %>% filter(allee.f=="without Allee effect") %>% 
-  mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
-  #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
-  filter(cociente>1) %>% 
-  ggscatterhist(x="initial.slope_log10",y="cociente_log10",
-                #color="bajoUmbral",
-                color="allee.f",
-                alpha=.4,size=3,
-                margin.plot="boxplot",
-                ggtheme=theme_bw(),
-                xlab="log10(1+initial slope)",
-                ylab="log10(1+slope after threshold/initial slope)",
-                palette = c("#14b74b"),
-                ylim=ylims,
-                xlim=xlims
-  )
-
-plot_slopes_sin_allee$sp
-
-ggarrange(print(plot_slopes_solo_condados),print(plot_slopes_con_allee$sp),print(plot_slopes_sin_allee$sp))
-
-plot_slopes$sp <-  plot_slopes_solo_condados$sp+
-  geom_abline(slope=0,intercept = log10(2),linetype="dotted") #+  
-# geom_mark_hull(concavity = 5,expand = -.00000001,aes(fill=allee.f)) 
+plot_slopes$sp = plot_slopes$sp + scale_shape_manual(values=c(4,20,20)) +
+  scale_size_manual(values=c(2,2,2))+scale_alpha_manual(values=c(.5,.3,.3)) +
+     geom_abline(slope=0,intercept = log10(2),linetype="dotted") #+  
+  
+# 
+# plot_slopes_solo_condados=
+#   sim_bb %>% filter(allee.f=="U.S. counties") %>% 
+#   mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
+#   #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
+#   filter(cociente>1) %>% 
+#   ggscatterhist(x="initial.slope_log10",y="cociente_log10",
+#                 #color="bajoUmbral",
+#                 color="allee.f",
+#                 alpha=.4,size=3,
+#                 margin.plot="boxplot",
+#                 ggtheme=theme_bw(),
+#                 xlab="log10(1+initial slope)",
+#                 ylab="log10(1+slope after threshold/initial slope)",
+#                 palette = c("#000080"),
+#                 ylim=ylims,
+#                 xlim=xlims
+#   )
+# 
+# plot_slopes_con_allee=
+#   sim_bb %>% filter(allee.f=="with Allee effect") %>% 
+#   mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
+#   #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
+#   filter(cociente>1) %>% 
+#   ggscatterhist(x="initial.slope_log10",y="cociente_log10",
+#                 #color="bajoUmbral",
+#                 color="allee.f",
+#                 alpha=.4,size=3,
+#                 margin.plot="boxplot",
+#                 ggtheme=theme_bw(),
+#                 xlab="log10(1+initial slope)",
+#                 ylab="log10(1+slope after threshold/initial slope)",
+#                 palette = c("#b33018"),
+#                 ylim=ylims,
+#                 xlim=xlims
+#   )
+# 
+# plot_slopes_con_allee$sp
+# 
+# plot_slopes_sin_allee=
+#   sim_bb %>% filter(allee.f=="without Allee effect") %>% 
+#   mutate_at(vars(contains("slope"),"cociente"),.funs = list("log10"=function(x) log10(1+x))) %>%
+#   #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
+#   filter(cociente>1) %>% 
+#   ggscatterhist(x="initial.slope_log10",y="cociente_log10",
+#                 #color="bajoUmbral",
+#                 color="allee.f",
+#                 alpha=.4,size=3,
+#                 margin.plot="boxplot",
+#                 ggtheme=theme_bw(),
+#                 xlab="log10(1+initial slope)",
+#                 ylab="log10(1+slope after threshold/initial slope)",
+#                 palette = c("#14b74b"),
+#                 ylim=ylims,
+#                 xlim=xlims
+#   )
+# 
+# plot_slopes_sin_allee$sp
+# 
+# ggarrange(print(plot_slopes),print(plot_slopes_con_allee$sp+legend("null")),print(plot_slopes_sin_allee$sp),
+#           nrow = 1,common.legend = T,legend="bottom")
+# 
+# plot_slopes$sp <-  plot_slopes_solo_condados$sp+
+#   geom_abline(slope=0,intercept = log10(2),linetype="dotted") #+  
+# # geom_mark_hull(concavity = 5,expand = -.00000001,aes(fill=allee.f)) 
 
 
 plot_slopes$sp$labels$colour=""
 plot_slopes$sp$labels$fill=""
 plot_slopes$yplot=plot_slopes$yplot + ylim(ylims)
 plot_slopes$xplot=plot_slopes$xplot + ylim(xlims)
+
 
 
 
