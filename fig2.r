a29e89a5afa1db5e091249602c23b18efe81e2b2
Álvaro Cabana <almadana@gmail.com>
2020-07-17 00:53:34 -0300

    fig 2 con leyendas en puntos condados

---
+++ fig2.r
@@ -0,0 +1,8 @@
+fig2 = ggarrange(plotsCondados + labs(tag="a"),
+          plot_slopes + labs(tag="b"), 
+          plotsPaises+labs(tag="c"),
+          plot_slopes_p + labs(tag="d"))
+
+fig2
+
+ggsave(fig2,file="fig2.pdf",width=14,height=9)
