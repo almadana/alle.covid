ca1b54e37eb10a7c2068a320e3faed42e0c001d2
dherrera1911 <dherrera1911@gmail.com>
2020-08-21 10:33:14 -0300

    emprolija codigo

---
+++ 10_plot_supplementary_figures.R
@@ -0,0 +1,228 @@
+library(ggpubr)
+library(ggforce)
+library(ggrepel)
+library(dplyr)
+
+# load data
+simFit <- readRDS("./generated_data/SIR_dynamics_fit.RDS") %>%
+  dplyr::mutate(allee = factor(ifelse(allee,"with Allee effect","without Allee effect")))
+
+countriesFit <- readRDS("./generated_data/countriesFit.RDS") %>%
+  dplyr::as_tibble(.) 
+
+countiesFit <- readRDS("./generated_data/countiesFit.RDS") %>%
+  dplyr::as_tibble(.) 
+
+dataFits <- list(counties = countiesFit, countries = countriesFit)
+
+
+####################
+# S4 make plots of distribution of slopes and correlation with population
+####################
+
+for (dataType in names(dataFits)) {
+  fitData <- dataFits[[dataType]]
+  fileName <- paste("./plots/S4_", dataType, "_fit_properties.pdf", sep = "")
+
+  slopeI_hist <- ggplot(fitData, aes(slopeI)) +
+    geom_histogram(color = "gray", fill="#4020ab") +
+    xlab("Initial slope") +
+    ylab("Count") +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size=0.5, linetype="solid"),
+          axis.line.y = element_line(size=0.5, linetype="solid"))
+  #mtext("a",side=3,at=-2.2,cex=1.6)
+
+  slopeF_hist <- ggplot(fitData, aes(slopeF)) +
+    geom_histogram(color = "gray", fill="#4020ab") +
+    xlab("Slope after threshold") +
+    ylab("Count") +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size=0.5, linetype="solid"),
+          axis.line.y = element_line(size=0.5, linetype="solid"))
+  #mtext("c",side=3,at=-2.2,cex=1.6)
+
+  breakPointInfected_hist <- ggplot(fitData, aes(log10(I.active))) +
+    geom_histogram(color = "gray", fill="#4020ab") +
+    xlab("Log10(Active at threshold)") +
+    ylab("Count") +
+    #scale_x_continuous(trans = "log10") +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size=0.5, linetype="solid"),
+          axis.line.y = element_line(size=0.5, linetype="solid"))
+  #mtext("b",side=3,at=-2.2,cex=1.6)
+
+  slopeI_Pop_Corr <- cor.test(fitData$slopeI, log10(fitData$Population),
+                             method = "pearson")
+  slopeI_Pop_plot <- ggplot(fitData, aes(x = log10(Population), y = slopeI)) +
+    geom_point(color = "#4020ab") +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size=0.5, linetype="solid"),
+          axis.line.y = element_line(size=0.5, linetype="solid")) +
+    geom_smooth(method = "lm", color = "red", se = FALSE) +
+    ylab("Initial slope") +
+    xlab("Log10(Population)")
+
+
+  slopeF_Pop_Corr <- cor.test(fitData$slopeF, log10(fitData$Population),
+                             method = "pearson")
+  slopeF_Pop_plot <- ggplot(fitData, aes(x = log10(Population), y = slopeF)) +
+    geom_point(color = "#4020ab") +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size=0.5, linetype="solid"),
+          axis.line.y = element_line(size=0.5, linetype="solid")) +
+    geom_smooth(method = "lm", color = "red", se = FALSE) +
+    ylab("Slope after threshold") +
+    xlab("Log10(Population)")
+
+
+  breakPoint_Pop_Corr <- cor.test(log10(fitData$I.active), log10(fitData$Population),
+                             method = "pearson")
+  breakPoint_Pop_plot <- ggplot(fitData, aes(x = log10(Population),
+                                                  y = log10(I.active))) +
+    geom_point(color = "#4020ab") +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          axis.line.x = element_line(size=0.5, linetype="solid"),
+          axis.line.y = element_line(size=0.5, linetype="solid")) +
+    geom_smooth(method = "lm", color = "red", se = FALSE) +
+    xlab("Log10(Active at threshold)") +
+    xlab("Log10(Population)")
+
+    
+  figureSupp4 <- ggarrange(slopeI_hist, slopeF_hist, breakPointInfected_hist,
+                   slopeI_Pop_plot, slopeF_Pop_plot, breakPoint_Pop_plot,
+                   widths = c(4,3), labels = "auto")
+
+  ggsave(figureSupp4, file = fileName, width = 14, height = 9)
+}
+
+
+################
+# S6 Histogram of weight of evidence of breaks for Allee and non-allee simulations
+###############
+
+# Plot histogram of the weight of evidence for breakpoint on simulations
+# with and without allee
+figS6a <- simFit %>%
+  ggplot(., aes(x = weighted.evidence, fill = allee))+
+  geom_histogram() +
+  facet_grid(~allee)+
+  labs(x="Weighted evidence for breakpoint",y="# of simulations",fill="")+
+  scale_fill_manual(values = c("#b33018", "#14b74b"))+
+  theme_classic() +
+  theme(strip.background = element_blank(), strip.text.x = element_blank(),
+    plot.title = element_text(hjust = 0.5,size=10),
+    text = element_text(size=12),
+    legend.position = "top")  
+
+
+#### plot density plot of change of slope as a function of initial slope.
+#### a plot of log10(1 + slopeRatio) vs log10(1 + slopeI) is done
+
+xlims=c(0,.45)
+ylims= c(-.01,2.5)
+
+plot_slopes_s6b <- simFit %>% 
+  dplyr::mutate_at(., vars(contains("slope"), "slopeRatio"),
+                   .funs = list("log10"=function(x) log10(1+x))) %>%
+  # keep only those with positive slope ratio
+  dplyr::filter(., !(is.nan(slopeRatio) | is.na(slopeRatio))) %>%
+  dplyr::filter(slopeRatio > 1) %>% 
+  ggscatterhist(x="slopeI_log10", y="slopeRatio_log10",
+                #color="bajoUmbral",
+                color="allee",
+                fill="allee",
+                size="allee",
+                #                alpha="allee",
+                group="allee",
+                alpha=.4,
+                margin.plot="boxplot",
+                ggtheme=theme_bw(),
+                xlab="log10(1+slope before breakout point)",
+                ylab="log10(1+slope after/slope before)",
+                palette = c("#b33018","#14b74b"),
+                ylim=ylims,
+                xlim=xlims)
+
+
+plot_slopes_s6b$sp <- simFit %>%
+  dplyr::filter(slopeRatio>1) %>%
+  ggplot(., aes(group = allee, fill = allee, x = log10(1+slopeI), y = log10(1+slopeRatio))) +
+  stat_density_2d(geom="polygon", aes(fill=allee, alpha=..level..), contour=T, bins=50) + 
+  scale_fill_manual(values = c("#b33018","#14b74b")) +
+  theme_classic() +
+  scale_color_manual(values=c("#b33018","#14b74b"))+
+  theme(legend.position = "none") +  
+  geom_abline(slope=0, intercept = log10(2)-.1, linetype="dotted") +
+  xlim(xlims) +
+  ylim(ylims) +
+  #  labs(x="log10(1+slope before breakout point)",y="log10(1+slope after/slope before)")
+  labs(x=expression(paste(Log10, "(", 1+mu[1], ")")),
+       y=expression(paste(Log10, "(", 1+frac(mu[1],mu[2]), ")"))) 
+
+plot_slopes_s6b$sp$labels$colour=""
+plot_slopes_s6b$sp$labels$fill=""
+plot_slopes_s6b$sp$labels$shape=""
+plot_slopes_s6b$sp$labels$size=""
+plot_slopes_s6b$sp$labels$alpha=""
+plot_slopes_s6b$yplot=plot_slopes_s6b$yplot + ylim(ylims) + theme(legend.position = "none")
+plot_slopes_s6b$xplot=plot_slopes_s6b$xplot + ylim(xlims)+ theme(legend.position = "none")
+plot_slopes_s6b$sp = plot_slopes_s6b$sp + guides(alpha="none",shape="none",colour="none") 
+
+plot_slopes_s6b <- print(plot_slopes_s6b)
+
+
+#### put histograms and density plots together
+figS6 <- ggarrange(figS6a, "./plots/S6_weight_evidence_sims.pdf",
+                   legend = "top",labels = "auto")
+
+ggsave(fig26, file = plotName, width = 8, height = 4)
+#ggsave(fig26,file="figs6.png",width=8,height=4)
+
+
+################
+# S? Histogram of weight of evidence of breaks for counties and countries
+###############
+
+weight.evid.vec <- c(countiesFit$weighted.evidence, countriesFit$weighted.evidence)
+geoVec <- c(rep("U.S. counties", nrow(countiesFit)),
+            rep("Countries & regions", nrow(countriesFit)))
+dataEvidPlot <- list()
+
+for (dataType in names(dataFits)) {
+  fitData <- dataFits[[dataType]]
+  fileName <- paste("./plots/SX_", dataType, "_weight_of_evidence.pdf", sep = "")
+
+  if (dataType == "counties") {
+    geoType <- "U.S. counties"  
+  } else if (dataType == "countries") {
+    geoType <- "Countries"  
+  }
+  fitData$geo <- geoType
+
+  # Plot histogram of the weight of evidence for breakpoint on data
+  dataEvidPlot[[dataType]] <- fitData %>%
+    ggplot(., aes(x = weighted.evidence)) +
+    geom_histogram() +
+    labs(x="Weighted evidence for breakpoint",y="# of simulations",fill="")+
+    scale_fill_manual(values = c("#b33018", "#14b74b"))+
+    theme_classic() +
+    theme(strip.background = element_blank(), strip.text.x = element_blank(),
+      plot.title = element_text(hjust = 0.5,size=10),
+      text = element_text(size=12),
+      legend.position = "top")  
+}
+
+figWeightEvid <- ggarrange(dataEvidPlot$counties, dataEvidPlot$countries,
+                           labels = "auto", nrow = 1)
+
+ggsave(figWeightEvid, file = "./plots/S6_weight_evidence_data.pdf",
+       width = 8, height = 4)
+
+
---
+++ 7_scatter_counties.R
@@ -0,0 +1,118 @@
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(ggforce)
+library(ggrepel)
+
+# load data
+countiesFit <- readRDS("./generated_data/countiesFit.RDS") %>%
+  dplyr::mutate(., slopeRatio = slopeF / slopeI)
+
+simFit <- readRDS("./generated_data/SIR_dynamics_fit.RDS") %>%
+  dplyr::mutate(allee = factor(ifelse(allee,"with Allee effect","without Allee effect")))
+
+fittedCounties <- as.character(countiesFit$Id)
+
+stateAbrev <- read.csv('county_abbreviations.csv')
+
+countiesCovid <- read.csv("us-counties.csv", colClasses = c("fips" = "character")) %>%
+  as_tibble(.) %>%
+  dplyr::filter(., county != "Unknown") %>%
+  dplyr::rename(., Id = fips) %>%
+  dplyr::group_by(Id) %>% 
+  dplyr::mutate(t = row_number(), peak = which.max(diff(cases))[1]) %>% 
+  dplyr::filter(t > 14, t <= peak) %>% 
+  dplyr::mutate(t = t - 14) %>%
+  merge(., stateAbrev, by = "state", all.x = T) %>%
+  dplyr::mutate(., county.full = paste(county, code, sep=", ")) %>%
+  dplyr::filter(., Id %in% fittedCounties) %>%
+  tidyr::as_tibble(.)
+
+county_short <- countiesCovid %>%
+  group_by(Id) %>%
+  summarize(county.full = county.full[1]) %>%
+  tidyr::as_tibble(.)
+
+countiesFit <- merge(countiesFit, county_short, by = "Id", all.x = T)
+
+countiesFit$label <- NA
+county4 <- NULL
+countiesFit <- countiesFit %>%
+  dplyr::mutate(label = ifelse(Id %in% county4, county.full, label)) %>%
+  as_tibble(.)
+
+countiesFit$allee = factor("U.S. counties", levels = c("U.S. counties", levels(simFit$allee)))
+simFit$Id <- seq(10000, 10000 + nrow(simFit)-1, 1)
+ 
+mergeCols <- c("Id", "allee", "slopeI", "slopeF", "slopeRatio")
+#("Id","allee","slopeI","slopeF","slopeRatio")
+
+sim_countiesFit <- merge(simFit[,mergeCols], countiesFit, by=mergeCols, all=T) %>%
+  as_tibble(.)
+
+sim_countiesFit$breakout <- sim_countiesFit$slopeRatio > 1
+sim_countiesFit$breakout_allee = interaction(sim_countiesFit$allee, sim_countiesFit$breakout)
+
+
+#### ----- PLOT FIGURA 2 ------
+
+xlims=c(0,.35)
+ylims= c(-.01,2)
+
+plot_slopes <- sim_countiesFit %>%
+  dplyr::mutate_at(vars(contains("slope"),"slopeRatio"), .funs = list("log10"=function(x) log10(1+x))) %>%
+  #filtra a los que no tiene quiebre de pendientes positivo (ratio mayor a uno)
+  #filter(slopeRatio>1) %>% 
+  ggscatterhist(x="slopeI_log10",y="slopeRatio_log10",
+                #color="bajoUmbral",
+                color="allee",
+                fill="allee",
+                shape="allee",
+                size="allee",
+#                alpha="allee",
+                group="allee",
+                alpha=.4,
+                margin.plot="boxplot",
+                ggtheme=theme_bw(),
+                xlab="log10(1+slope before breakout point)",
+                ylab="log10(1+slope after/slope before)",
+                palette = c("#000080","#b33018","#14b74b"),
+                ylim=ylims,
+                xlim=xlims)
+
+plot_slopes$sp <- simFit %>%
+  filter(slopeRatio>1) %>%
+  ggplot(., aes(group=allee, fill=allee, x=log10(1+slopeI), y=log10(1+slopeRatio))) +
+  stat_density_2d(geom="polygon", aes(fill=allee, alpha=..level..), contour=T, bins=14) + 
+  geom_point(shape=20, data=(countiesFit %>% filter(slopeRatio>1)), alpha=1, aes(col=allee)) +
+  scale_fill_manual(values = c("#FFFFFF","#b33018","#14b74b")) +
+  theme_classic() +
+  scale_color_manual(values=c("#000080","#b33018","#14b74b"))+
+  theme(legend.position = "top") +  
+  geom_abline(slope=0,intercept = log10(2)-.1,linetype="dotted") +
+  geom_text_repel(data=sim_countiesFit, aes(label=label), size=3)+
+  annotate("text", x=0.03, y=log10(2)+0.1, label="equal slopes", size=3) +
+  xlim(xlims) +
+  ylim(ylims) +
+#  labs(x="log10(1+slope before breakout point)",y="log10(1+slope after/slope before)")
+  labs(x=expression(paste(Log10, "(", 1+mu[1], ")")),
+       y=expression(paste(Log10, "(", 1+frac(mu[1],mu[2]), ")"))) 
+
+
+plot_slopes$sp$labels$colour=""
+plot_slopes$sp$labels$fill=""
+plot_slopes$sp$labels$shape=""
+plot_slopes$sp$labels$size=""
+plot_slopes$sp$labels$alpha=""
+plot_slopes$yplot <- plot_slopes$yplot + ylim(ylims)
+plot_slopes$xplot <- plot_slopes$xplot + ylim(xlims)
+plot_slopes$sp <- plot_slopes$sp + guides(alpha="none",shape="none",colour="none") 
+
+plot_slopes <- print(plot_slopes)
+
+saveRDS(list(scatterSlopesCounties = plot_slopes), "./generated_data/counties_scatter_plot.RDS")
+ggsave(plot_slopes, file="fig2_scatter_condados.pdf", width=5, height=4)
+#ggsave(plot_slopes, file="fig2_scatter_condados.png", width=5, height=4)
+
+
